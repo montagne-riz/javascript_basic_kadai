c2aa3f969a5b02dc7730d013f8878e585eae3c7e
montagne-riz <montagne.riz16@gmail.com>
2025-03-08 17:13:14 +0900

    javascriptを変更

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,13 +1,2 @@
 const today = new Date();
-
-console.log(today.getFullYear());
-
-
-
-Takato Kanehara
-9:52
-const today = new Date();
-Takato Kanehara
-9:58
-const year = today.getFullYear();
-console.log(year + "年" + month + "月" + date + "日")
+console.log (today);
